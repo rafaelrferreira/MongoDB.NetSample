c6f45daabf87b79aed0e9f449276349fd7c01a43
RAFAEL REIS FERREIRA <rafaelrferreira@gmail.com>
2016-08-25 10:00:23 -0300

    Primeira versão
    Exemplo de consulta com MongoDB Driver e .Net Console App

---
+++ M101DotNet/Program.cs
@@ -0,0 +1,32 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M101DotNet {
+    class Program {
+        static void Main(string[] args) {
+            var connectionString = "mongodb://localhost:27017";
+            var client = new MongoClient(connectionString);
+
+            var db = client.GetDatabase("test");
+            var collection = db.GetCollection<BsonDocument>("name");
+            var list = collection.Find(new BsonDocument()).ToList();
+
+            Console.WriteLine();
+            foreach(var document in list) {
+                Console.WriteLine(document["name"]);
+            }
+            Console.WriteLine("Pressione ENTER");
+            Console.ReadLine();
+        }
+    }
+
+    public class Person {
+        public ObjectId _id { get; set; }
+        public  string name { get; set; }
+    }
+}
